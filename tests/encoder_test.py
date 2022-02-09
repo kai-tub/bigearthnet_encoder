292e325be6ec0f02e632b3c11bd2a12c9aa3b4dc
Kai Norman Clasen <46302524+kai-tub@users.noreply.github.com>
2022-02-09 10:09:03 +0100

    Add tests for S1S2 LMDB generator (#13)
    - Update generator function builder name
    - Use black formatting
    - Add some more function annotations
    - Updated/Added a few docstrings

--- tests/encoder_test.py
+++ tests/encoder_test.py
@@ -6,14 +6,17 @@ from pathlib import Path
 import tempfile
 from bigearthnet_patch_interface.s1_interface import BigEarthNet_S1_Patch
 from bigearthnet_patch_interface.s2_interface import BigEarthNet_S2_Patch
+from bigearthnet_patch_interface.merged_interface import BigEarthNet_S1_S2_Patch
 
 from bigearthnet_encoder.encoder import (
     write_S1_lmdb_raw,
     write_S2_lmdb_raw,
+    write_S1_S2_lmdb_raw,
     write_S2_lmdb,
     write_S1_lmdb,
     write_S1_lmdb_with_lbls,
     write_S2_lmdb_with_lbls,
+    write_S1_S2_lmdb_with_lbls,
     _write_lmdb,
 )
 
@@ -25,7 +28,7 @@ TEST_S2_ENTRIES = 3
 TEST_S1_ROOT = Path(__file__).parent / "s1-tiny"
 TEST_S1_FOLDER = TEST_S1_ROOT / "S1A_IW_GRDH_1SDV_20170613T165043_33UUP_61_39"
 TEST_S1_FILE = TEST_S1_FOLDER / "S1A_IW_GRDH_1SDV_20170613T165043_33UUP_61_39_VV.tif"
-TEST_S1_ENTRIES = 3
+TEST_S1_ENTRIES = 6
 
 
 def test_s2_tiff_dir_to_ben():
@@ -43,18 +46,19 @@ def test_empty_dir():
         _write_lmdb([], lambda x: x)
 
 
-def _lmdb_tester(lmdb_path, is_sentinel2, size, test_attrs=None):
-    key_re_checker = BEN_S2_RE if is_sentinel2 else BEN_S1_RE
-    patch_class = BigEarthNet_S2_Patch if is_sentinel2 else BigEarthNet_S1_Patch
+def _lmdb_tester(
+    lmdb_path, is_key_sentinel2_dir, expected_class, size, test_attrs=None
+):
+    key_re_checker = BEN_S2_RE if is_key_sentinel2_dir else BEN_S1_RE
     env = lmdb.open(lmdb_path)
     with env.begin() as txn:
         assert env.stat()["entries"] == size
         for enc_patch_name, binary_patch_data in txn.cursor():
             patch_name = enc_patch_name.decode()
             assert key_re_checker.fullmatch(patch_name)
-            patch_data = patch_class.loads(binary_patch_data)
+            patch_data = expected_class.loads(binary_patch_data)
             # just highlighting that the decoding has been succesfull
-            assert isinstance(patch_data, patch_class)
+            assert isinstance(patch_data, expected_class)
             if test_attrs is not None:
                 for attr in test_attrs:
                     assert hasattr(patch_data, attr)
@@ -63,22 +67,53 @@ def _lmdb_tester(lmdb_path, is_sentinel2, size, test_attrs=None):
 def test_write_S2_lmdb_raw():
     with tempfile.TemporaryDirectory() as tmpdir:
         write_S2_lmdb_raw(TEST_S2_ROOT, lmdb_path=tmpdir)
-        _lmdb_tester(tmpdir, True, TEST_S2_ENTRIES)
+        _lmdb_tester(tmpdir, True, BigEarthNet_S2_Patch, TEST_S2_ENTRIES)
 
 
 def test_write_S1_lmdb_raw():
     with tempfile.TemporaryDirectory() as tmpdir:
         write_S1_lmdb_raw(TEST_S1_ROOT, lmdb_path=tmpdir)
-        _lmdb_tester(tmpdir, False, TEST_S1_ENTRIES)
+        _lmdb_tester(tmpdir, False, BigEarthNet_S1_Patch, TEST_S1_ENTRIES)
+
+
+def test_write_S1_S2_lmdb_raw():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        write_S1_S2_lmdb_raw(TEST_S1_ROOT, TEST_S2_ROOT, lmdb_path=tmpdir)
+        _lmdb_tester(tmpdir, True, BigEarthNet_S1_S2_Patch, TEST_S2_ENTRIES)
 
 
 def test_write_S2_lmdb_with_lbls():
     with tempfile.TemporaryDirectory() as tmpdir:
         write_S2_lmdb_with_lbls(TEST_S2_ROOT, lmdb_path=tmpdir)
-        _lmdb_tester(tmpdir, True, TEST_S2_ENTRIES, ["labels", "new_labels"])
+        _lmdb_tester(
+            tmpdir,
+            True,
+            BigEarthNet_S2_Patch,
+            TEST_S2_ENTRIES,
+            ["labels", "new_labels"],
+        )
 
 
+# TODO: Find the correctly matching data
 def test_write_S1_lmdb_with_lbls():
     with tempfile.TemporaryDirectory() as tmpdir:
         write_S1_lmdb_with_lbls(TEST_S1_ROOT, lmdb_path=tmpdir)
-        _lmdb_tester(tmpdir, False, TEST_S1_ENTRIES, ["labels", "new_labels"])
+        _lmdb_tester(
+            tmpdir,
+            False,
+            BigEarthNet_S1_Patch,
+            TEST_S1_ENTRIES,
+            ["labels", "new_labels"],
+        )
+
+
+def test_write_S1_S2_lmdb_with_lbls():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        write_S1_S2_lmdb_with_lbls(TEST_S1_ROOT, TEST_S2_ROOT, lmdb_path=tmpdir)
+        _lmdb_tester(
+            tmpdir,
+            True,
+            BigEarthNet_S1_S2_Patch,
+            TEST_S2_ENTRIES,
+            ["labels", "new_labels"],
+        )
